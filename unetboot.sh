b270ea8ae2a8d355c9861a680a3a3cc725468086
Sazid Hasan Milon <36078194+shmilon@users.noreply.github.com>
2021-02-01 05:11:54 +0600

    Add files via upload

---
+++ unetboot.sh
@@ -0,0 +1,11 @@
+#!bin/sh
+
+sudo add-apt-repository ppa:gezakovacs/ppa
+sudo apt-get update
+sudo apt-get install unetbootin
+
+
+#Binaries: 32-bit binary 64-bit binary
+#To run these binaries, download them and run the command chmod +x ./unetbootin-linux, or go to Properties->Permissions and check "Execute"),
+
+#then start the application by running ./unetbootin-linux
